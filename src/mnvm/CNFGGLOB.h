805722e892dc998dd92d4d86015220be9ca8d971
Jesús A. Álvarez <zydeco@namedfork.net>
2008-12-12 18:11:06 +0000

    removed unused defines

--- src/mnvm/CNFGGLOB.h
+++ src/mnvm/CNFGGLOB.h
@@ -3,10 +3,6 @@
 #define BigEndianUnaligned 0
 #define MayInline inline
 #define MayNotInline __attribute__((noinline))
-
-#define cIncludeUnused 0
-
-#define MyFrameSkip 0
 #define MySoundEnabled 1
 #define CurEmu kEmuPlus
 #define NumDrives 8
