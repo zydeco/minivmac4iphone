dcb54546e73e8e167adc97f8a94720ada5b259c5
Jesús A. Álvarez <zydeco@namedfork.net>
2008-12-13 00:32:05 +0000

    orientation

--- src/prefix.h
+++ src/prefix.h
@@ -2,6 +2,10 @@
 
 #import <UIKit/UIKit.h>
 
+// stuff missing from devteam headers
+
+#define UIInterfaceOrientationLandscapeRight 3
+
 @interface UIApplication (extensions)
 - (void) setStatusBarOrientation:(int)fp8 animated:(BOOL)fp12;
 @end
